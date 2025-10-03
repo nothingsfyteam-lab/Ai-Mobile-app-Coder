a4f54b9194840dabf42f6c00f229486b38b4033f
nothingsfyteam-lab <nothingsfyteam@gmail.com>
2025-10-03 17:03:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyDbVo-1rY1-RP85n20fyuhKZ4FZENWzUwk),
+        'process.env.AIzaSyDbVo-1rY1-RP85n20fyuhKZ4FZENWzUwk': JSON.stringify(env.AIzaSyDbVo-1rY1-RP85n20fyuhKZ4FZENWzUwk)
       },
       resolve: {
         alias: {
